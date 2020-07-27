f1129baa9a0de4eedf4f1e1583c55a5525b73e83
guoshenga <guoshenga@digitalchina.com>
2020-07-27 18:45:46 +0800

    init project

---
+++ addNum/AddNum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddNum
+{
+    public class AddNum
+    {
+        public int AddIntNum(int a , int b)
+        {
+            return a + b;
+        }
+
+        public int AddIntNumPlus(int a, int b)
+        {
+            return AddIntNum(a,b)+1;
+        }
+    }
+}
